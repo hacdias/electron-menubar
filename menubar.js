22852f48ad3e623a60e9529d72b3eaba78e7986c
Henrique Dias <hacdias@gmail.com>
2017-12-23 19:46:09 +0000

    feat: add positioner

---
+++ menubar.js
@@ -0,0 +1,209 @@
+const path = require('path')
+const Positioner = require('./positioner')
+const merge = require('lodash.merge')
+const {EventEmitter} = require('events')
+const electron = require('electron')
+const {app, Tray, BrowserWindow} = electron
+
+const defaults = {
+  preloadWindow: false,
+  showDockIcon: false,
+  showOnRightClick: false,
+  showOnAllWorkspaces: true,
+  tooltip: '',
+  window: {
+    width: 400,
+    height: 400,
+    show: false,
+    frame: false
+  }
+}
+
+/**
+ * Class Menubar.
+ */
+class Menubar extends EventEmitter {
+  constructor (opts) {
+    super()
+
+    if (typeof opts !== 'object') {
+      opts = {}
+    }
+
+    opts = merge(defaults, opts)
+
+    if (!path.isAbsolute(opts.dir)) {
+      opts.dir = path.resolve(opts.dir)
+    }
+
+    if (!opts.index) {
+      opts.index = `file://${path.join(app.getAppPath(), 'index.html')}`
+    }
+
+    if (!opts.icon) {
+      opts.icon = path.join(app.getAppPath(), 'icon.png')
+    }
+
+    this.opts = opts
+    this.ready = false
+    this.cachedBounds = null
+    this.window = null
+
+    if (app.isReady()) {
+      this._appReady()
+    } else {
+      app.on('ready', this._appReady.bind(this))
+    }
+  }
+
+  /**
+   * This sets up the tray. Must only be called once.
+   *
+   * @returns {void}
+   */
+  _appReady () {
+    if (app.dock && !this.opts.showDockIcon) {
+      app.dock.hide()
+    }
+
+    let clickEvent = this.opts.showOnRightClick ? 'right-click' : 'click'
+
+    this.tray = new Tray(this.opts.icon)
+    this.tray.on(clickEvent, this._clicked.bind(this))
+    this.tray.on('double-click', this._clicked.bind(this))
+    this.tray.setToolTip(this.opts.tooltip)
+    this.tray.setHighlightMode('never')
+
+    if (this.opts.preloadWindow) {
+      this._createWindow()
+    }
+
+    this.ready = true
+    this.emit('ready')
+  }
+
+  /**
+   * Creates the Window. Must only be called once.
+   *
+   * @returns {void}
+   */
+  _createWindow () {
+    this.emit('create-window')
+
+    this.window = new BrowserWindow(this.opts.window)
+
+    if (this.opts.showOnAllWorkspaces !== false) {
+      this.window.setVisibleOnAllWorkspaces(true)
+    }
+
+    this.window.on('close', () => {
+      this.window = null
+    })
+
+    this.window.on('blur', () => {
+      if (!this.window.isAlwaysOnTop()) this.hide()
+    })
+
+    this.window.loadURL(this.opts.index)
+    this.emit('after-create-window')
+  }
+
+  /**
+   * Triggered when clicking the tray icon.
+   *
+   * @param {Event} event
+   * @param {Rectangle} bounds
+   *
+   * @returns {void}
+   */
+  _clicked (event, bounds) {
+    // Hide the window if clicking with ALT/SHIFT/CTRL or Meta keys.
+    if (event.altKey || event.shiftKey || event.ctrlKey || event.metaKey) {
+      return this.hide()
+    }
+
+    // If there is a window and it is already visible, hide it.
+    if (this.window && this.window.isVisible()) {
+      return this.hide()
+    }
+
+    this.cachedBounds = bounds || this.cachedBounds
+    this.show()
+  }
+
+  /**
+   * Informs if the Menubar is already ready.
+   *
+   * @returns {void}
+   */
+  isReady () {
+    return this.ready
+  }
+
+  /**
+   * Hides the Window.
+   *
+   * @returns {void}
+   */
+  hide () {
+    this.tray.setHighlightMode('never')
+    if (!this.window) return
+    this.emit('hide')
+    this.window.hide()
+  }
+
+  /**
+   * Shows the Window.
+   *
+   * @returns {void}
+   */
+  show () {
+    this.tray.setHighlightMode('always')
+    if (!this.window) {
+      this._createWindow()
+    }
+
+    if (!this.cachedBounds) {
+      this.cachedBounds = this.tray.getBounds()
+    }
+
+    const pos = Positioner.calculate(this.window.getBounds(), this.cachedBounds)
+    this._setPositionAndShow(pos)
+  }
+
+  _setPositionAndShow (pos) {
+    pos.x = (this.opts.window.x !== undefined) ? this.opts.window.x : pos.x
+    pos.y = (this.opts.window.y !== undefined) ? this.opts.window.y : pos.y
+
+    this.window.setPosition(pos.x, pos.y)
+    this.window.show()
+    this.emit('show')
+  }
+
+  /**
+   * Get the Position by Cursor.
+   *
+   * @return {Point}
+   */
+  _positionByCursor () {
+    const screen = electron.screen
+    const display = screen.getDisplayNearestPoint(screen.getCursorScreenPoint())
+    const cursor = screen.getCursorScreenPoint()
+    const window = this.window.getBounds()
+
+    let x = cursor.x - (window.width / 2)
+    let y = cursor.y - (window.height / 2)
+
+    if (x + window.width > display.bounds.width) {
+      x = display.bounds.width - window.width
+    }
+
+    if (y + window.height > display.bounds.height) {
+      y = display.bounds.height - window.height
+    }
+
+    return { x, y }
+  }
+}
+
+module.exports = Menubar
---
+++ positioner.js
@@ -0,0 +1,206 @@
+const electron = require('electron')
+
+/**
+ * Calculates the x position of the tray window
+ *
+ * @param {Rectangle} windowBounds - electron BrowserWindow bounds of tray window to position
+ * @param {Rectangle} trayBounds - tray bounds from electron Tray.getBounds()
+ * @param {string} [align] - align left|center|right, default: center
+ * @return {integer} - calculated x position
+ */
+function calculateXAlign (windowBounds, trayBounds, align) {
+  const display = getDisplay()
+  let x
+
+  switch (align) {
+    case 'right':
+      x = trayBounds.x
+      break
+    case 'left':
+      x = trayBounds.x + trayBounds.width - windowBounds.width
+      break
+    case 'center':
+    default:
+      x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+  }
+
+  if (x + windowBounds.width > display.bounds.width && align !== 'left') {
+    // if window would overlap on right side align it to the end of the screen
+    x = display.bounds.width - windowBounds.width
+  } else if (x < 0 && align !== 'right') {
+    // if window would overlap on the left side align it to the beginning
+    x = 0
+  }
+
+  return x
+}
+
+/**
+ * Calculates the y position of the tray window
+ *
+ * @param {Rectangle} windowBounds - electron BrowserWindow bounds
+ * @param {Rectangle} trayBounds - tray bounds from electron Tray.getBounds()
+ * @param {string} [align] - align up|middle|down, default: down
+ * @return {integer} - calculated y position
+ */
+function calculateYAlign (windowBounds, trayBounds, align) {
+  const display = getDisplay()
+  let y
+
+  switch (align) {
+    case 'up':
+      y = trayBounds.y + trayBounds.height - windowBounds.height
+      break
+    case 'center':
+      y = Math.round((trayBounds.y + (trayBounds.height / 2)) - (windowBounds.height / 2))
+      break
+    case 'down':
+    default:
+      y = trayBounds.y
+  }
+
+  if (y + windowBounds.height > display.bounds.height && align !== 'up') {
+    y = trayBounds.y + trayBounds.height - windowBounds.height
+  } else if (y < 0 && align !== 'down') {
+    y = 0
+  }
+
+  return y
+}
+
+/**
+ * Calculates the position of the tray window based on current cursor position
+ * This method is used on linux where trayBounds are not available
+ *
+ * @param {Rectangle} windowBounds - electron BrowserWindow bounds of tray window to position
+ * @param {Eelectron.Display} display - display on which the cursor is currently
+ * @param {Point} cursor - current cursor position
+ * @return {Point} - Calculated point {x, y} where the window should be positioned
+ */
+function calculateByCursorPosition (windowBounds, display, cursor) {
+  let x = cursor.x
+  let y = cursor.y
+
+  if (x + windowBounds.width > display.bounds.width) {
+    // if window would overlap on right side of screen, align it to the left of the cursor
+    x -= windowBounds.width
+  }
+
+  if (y + windowBounds.height > display.bounds.height) {
+    // if window would overlap at bottom of screen, align it up from cursor position
+    y -= windowBounds.height
+  }
+
+  return {
+    x: x,
+    y: y
+  }
+}
+
+function getScreen () {
+  return electron.screen
+}
+
+function getCursorPosition () {
+  const screen = getScreen()
+  return screen.getCursorScreenPoint()
+}
+
+/**
+ * Get the display nearest the current cursor position
+ *
+ * @return {Electron.Display} - the display closest to the current cursor position
+ */
+function getDisplay () {
+  const screen = getScreen()
+  return screen.getDisplayNearestPoint(screen.getCursorScreenPoint())
+}
+
+/**
+ * Calculates the position of the tray window
+ *
+ * @return {string} - the position of the taskbar (top|right|bottom|left)
+ */
+function getTaskbarPosition () {
+  const display = getDisplay()
+
+  if (display.workArea.y > 0) {
+    return 'top'
+  } else if (display.workArea.x > 0) {
+    return 'left'
+  } else if (display.workArea.width === display.bounds.width) {
+    return 'bottom'
+  }
+
+  return 'right'
+}
+
+class Positioner {
+  /**
+   * Calculates the position of the tray window
+   *
+   * @param {Rectangle} windowBounds - electron BrowserWindow bounds of tray window to position
+   * @param {Rectangle} trayBounds - tray bounds from electron Tray
+   * @param {Object} [alignment] - alignment of window to tray
+   * @param {string} [alignment.x] - x align if tray bar is on top or bottom (left|center|right),
+      default: center
+   * @param {string} [alignment.y] - y align if tray bar is left or right (up|middle|down),
+      default: down
+   * @return {Point} - Calculated point {x, y} where the window should be positioned
+   */
+  static calculate (windowBounds, trayBounds, alignment) {
+    if (process.platform === 'linux') {
+      const cursor = getCursorPosition()
+      return calculateByCursorPosition(windowBounds, getDisplay(), cursor)
+    }
+
+    const _alignment = alignment || {}
+    const taskbarPosition = getTaskbarPosition()
+    const display = getDisplay()
+    let x
+    let y
+
+    switch (taskbarPosition) {
+      case 'left':
+        x = display.workArea.x
+        y = calculateYAlign(windowBounds, trayBounds, _alignment.y)
+        break
+      case 'right':
+        x = display.workArea.width - windowBounds.width
+        y = calculateYAlign(windowBounds, trayBounds, _alignment.y)
+        break
+      case 'bottom':
+        x = calculateXAlign(windowBounds, trayBounds, _alignment.x)
+        y = display.workArea.height - windowBounds.height
+        break
+      case 'top':
+      default:
+        x = calculateXAlign(windowBounds, trayBounds, _alignment.x)
+        y = display.workArea.y
+    }
+
+    return {
+      x,
+      y
+    }
+  }
+
+  /**
+   * Calculates the position of the tray window
+   *
+   * @param {BrowserWindow} window - window to position
+   * @param {Rectangle} trayBounds - tray bounds from electron Tray
+   * @param {Object} [alignment] - alignment of window to tray
+   * @param {string} [alignment.x] - x align if tray bar is on top or bottom (left|center|right),
+      default: center
+   * @param {string} [alignment.y] - y align if tray bar is left or right (up|middle|down),
+      default: down
+   * @return {Void}
+   */
+  static position (window, trayBounds, alignment) {
+    const position = this.calculate(window.getBounds(), trayBounds, alignment)
+    window.setPosition(position.x, position.y, false)
+  }
+}
+
+module.exports = Positioner
