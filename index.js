53798cb90629e46af5a25ed1b855da74fa113f85
Henrique Dias <hacdias@gmail.com>
2017-12-23 10:15:58 +0000

    feat: 1st version
    updates
    
    fix: unstable
    
    feat: 1st stable version based on menubar

---
+++ index.js
@@ -0,0 +1,156 @@
+const path = require('path')
+const {EventEmitter} = require('events')
+const {app, Tray, BrowserWindow} = require('electron')
+const Positioner = require('electron-positioner')
+
+const defaults = {
+  dir: app.getAppPath(),
+  windowPosition: (process.platform === 'win32') ? 'trayBottomCenter' : 'trayCenter',
+  showDockIcon: false,
+  tooltip: '',
+  window: {
+    width: 400,
+    height: 400,
+    show: false,
+    frame: false
+  }
+}
+
+module.exports = class Menubar extends EventEmitter {
+  constructor (opts) {
+    super()
+
+    if (typeof opts === 'string') {
+      opts = {
+        dir: opts
+      }
+    }
+
+    opts = Object.assign({}, defaults, opts)
+
+    if (!path.isAbsolute(opts.dir)) {
+      opts.dir = path.resolve(opts.dir)
+    }
+
+    if (!opts.index) {
+      opts.index = `file://${path.join(opts.dir, 'index.html')}`
+    }
+
+    this.opts = opts
+
+    if (app.isReady()) this._appReady()
+    else app.on('ready', this._appReady.bind(this))
+  }
+
+  _appReady () {
+    if (app.dock && !this.opts.showDockIcon) app.dock.hide()
+
+    var iconPath = this.opts.icon || path.join(this.opts.dir, 'IconTemplate.png')
+
+    this.cachedBounds = null
+    var defaultClickEvent = this.opts.showOnRightClick ? 'right-click' : 'click'
+
+    this.tray = this.opts.tray || new Tray(iconPath)
+    this.tray.on(defaultClickEvent, this._clicked.bind(this))
+    this.tray.on('double-click', this._clicked.bind(this))
+    this.tray.setToolTip(this.opts.tooltip)
+
+    this.supportsTrayHighlightState = false
+    try {
+      this.tray.setHighlightMode('never')
+      this.supportsTrayHighlightState = true
+    } catch (e) {}
+
+    if (this.opts.preloadWindow) {
+      this._createWindow()
+    }
+
+    this.emit('ready')
+  }
+
+  _clicked (e, bounds) {
+    if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) return this.hideWindow()
+    if (this.window && this.window.isVisible()) return this.hideWindow()
+    this.cachedBounds = bounds || this.cachedBounds
+    this.showWindow(this.cachedBounds)
+  }
+
+  createWindow () {
+    this.emit('create-window')
+
+    this.window = new BrowserWindow(this.opts.window)
+    this.positioner = new Positioner(this.window)
+
+    this.window.on('blur', function () {
+      this.opts.alwaysOnTop ? this.emitBlur() : this.hideWindow()
+    })
+
+    if (this.opts.showOnAllWorkspaces !== false) {
+      this.window.setVisibleOnAllWorkspaces(true)
+    }
+
+    this.window.on('close', this.windowClear.bind(this))
+    this.window.loadURL(this.opts.index)
+    this.emit('after-create-window')
+  }
+
+  setOption (opt, val) {
+    this.opts[opt] = val
+  }
+
+  getOption (opt) {
+    return this.opts[opt]
+  }
+
+  hideWindow () {
+    if (this.supportsTrayHighlightState) this.tray.setHighlightMode('never')
+    if (!this.window) return
+    this.emit('hide')
+    this.window.hide()
+    this.emit('after-hide')
+  }
+
+  windowClear () {
+    delete this.window
+    this.emit('after-close')
+  }
+
+  emitBlur () {
+    this.emit('focus-lost')
+  }
+
+  showWindow (trayPos) {
+    if (this.supportsTrayHighlightState) this.tray.setHighlightMode('always')
+    if (!this.window) {
+      this.createWindow()
+    }
+
+    this.emit('show')
+
+    if (trayPos && trayPos.x !== 0) {
+      // Cache the bounds
+      this.cachedBounds = trayPos
+    } else if (this.cachedBounds) {
+      // Cached value will be used if showWindow is called without bounds data
+      trayPos = this.cachedBounds
+    } else if (this.tray.getBounds) {
+      // Get the current tray bounds
+      trayPos = this.tray.getBounds()
+    }
+
+    // Default the window to the right if `trayPos` bounds are undefined or null.
+    var noBoundsPosition = null
+    if ((trayPos === undefined || trayPos.x === 0) && this.opts.windowPosition.substr(0, 4) === 'tray') {
+      noBoundsPosition = (process.platform === 'win32') ? 'bottomRight' : 'topRight'
+    }
+
+    var position = this.positioner.calculate(noBoundsPosition || this.opts.windowPosition, trayPos)
+
+    var x = (this.opts.x !== undefined) ? this.opts.x : position.x
+    var y = (this.opts.y !== undefined) ? this.opts.y : position.y
+
+    this.window.setPosition(x, y)
+    this.window.show()
+    this.emit('after-show')
+  }
+}
